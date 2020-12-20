1cc775a5a56fbeb08f141dbd9576eb03516c93e3
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-12-20 12:03:47 +0530

    ADDED:TestCase for getting the employeeList using RestSharpApi

--- EmployeePayRollMsTestRestSharp/RestSharpTest.cs
+++ EmployeePayRollMsTestRestSharp/RestSharpTest.cs
@@ -1,13 +1,43 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Net;
 
 namespace EmployeePayRollMsTestRestSharp
 {
     [TestClass]
     public class RestSharpTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        RestClient client;
+
+        [TestInitialize]
+        public void Setup()
         {
+            client = new RestClient("http://localhost:4000");
+        }
+
+        private IRestResponse getEmployeeList()
+        {
+            //arrange
+            RestRequest request = new RestRequest("/employees", Method.GET);
+            //act
+            IRestResponse response = client.Execute(request);
+            return response;
+        }
+
+        [TestMethod]
+        public void OnCallingGet_ShouldReturnEmployeeList()
+        {
+            IRestResponse response = getEmployeeList();
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
+            List<Employee> dataResponse = JsonConvert.DeserializeObject<List<Employee>>(response.Content);
+            Assert.AreEqual(11, dataResponse.Count);
+            foreach(Employee employee in dataResponse)
+            {
+                Console.WriteLine("Id:" + employee.id + "\nName:" + employee.name + "\nSalary:" + employee.salary);
+            }
         }
     }
 }
